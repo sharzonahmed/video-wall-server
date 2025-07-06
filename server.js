1dc5d5634120226543f616f877ad5640040d9908
sharzonahmed <sharzonahmed@gmail.com>
2025-07-07 00:23:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const WebSocket = require("ws");
 const server = new WebSocket.Server({ port: 3000 });
 
+let master = null;
+
 server.on("connection", (socket) => {
+  console.log("Client connected");
+
   socket.on("message", (msg) => {
-    server.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(msg);
-      }
-    });
+    if (msg === "set-master") {
+      master = socket;
+      console.log("Master set");
+      return;
+    }
+
+    // Only broadcast if the master is sending
+    if (socket === master) {
+      server.clients.forEach((client) => {
+        if (client !== master && client.readyState === WebSocket.OPEN) {
+          client.send(msg);
+        }
+      });
+    }
+  });
+
+  socket.on("close", () => {
+    if (socket === master) {
+      master = null;
+      console.log("Master disconnected");
+    }
   });
 });
 
